8b2e1b716d3b611becb52639801805945ee12cab
Muhammad Hassaan Khan <hk7866@yahoo.com>
2025-07-21 05:14:44 +0500

    Delete signup page directory

--- signup page/signup.js
+++
@@ -1,5 +0,0 @@
-// JavaScript for Join as Vendor page
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Join as Vendor script loaded!');
-    // Add any specific JavaScript functionality here, e.g., form validation
-});
