b5a70c92a479e0502628ac394740ef7e5d62f52b
Muhammad Hassaan Khan <hk7866@yahoo.com>
2025-07-21 05:16:18 +0500

    Frontend of vendor flow

---
+++ signup.js
@@ -0,0 +1,5 @@
+// JavaScript for Join as Vendor page
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Join as Vendor script loaded!');
+    // Add any specific JavaScript functionality here, e.g., form validation
+});
